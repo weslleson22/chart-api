2f9cdb18c6c6a8ce79d4a0ef735d66f809c77a73
Wesleson22 <wesllesonsowza@gmail.com>
2023-08-12 09:23:06 -0300

    Versão ok 2 do chart

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div className="bg-neutral-50"><Component {...pageProps} /></div>
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,17 @@
+import { useState, useEffect } from "react";
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import axios from 'axios';
+import TypingAnimation from "../components/TypingAnimation";
 
-import {useState, UseEffect} from 'react';
-import { errors } from 'celebrate';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [inputValue, setInputValue] = useState('');
   const [chatLog, setChatLog] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -25,11 +24,7 @@ export default function Home() {
   }
 
   const sendMessage = (message) => {
-    const url = 'https://api.openai.com/v1/chat/completions';
-    const headers = {
-      'Content-type': 'application/json',
-      'Authorization': `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`
-    };
+    const url = '/api/chat';
 
     const data = {
       model: "gpt-3.5-turbo-0301",
@@ -38,7 +33,7 @@ export default function Home() {
 
     setIsLoading(true);
 
-    axios.post(url, data, {headers: headers}).then((response) => {
+    axios.post(url, data).then((response) => {
       console.log(response);
       setChatLog((prevChatLog) => [...prevChatLog, { type: 'bot', message: response.data.choices[0].message.content }])
       setIsLoading(false);
@@ -47,19 +42,43 @@ export default function Home() {
       console.log(error);
     })
   }
+
   return (
-        <>
-        <h1>CharGPT</h1>
-        {
-          chatLog.map((message, index)=>(
-            <div key="index">{message.message}</div>
-          ))
-        }
-        <form onSubmit={handleSubmit}>
-          <input placeholder="Digite a sua pergunta aqui..." value={inputValue} onChange={(e) => setInputValue(e.target.value)}         
-          />
-          <button type="submit">Enviar</button>
+    <div className="container mx-auto max-w-[700px]">
+      <div className="flex flex-col h-screen bg-neutral-50">
+        <h1 className="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">Chat Me-Alzheimer</h1>
+        <div className="flex-grow p-6">
+          <div className="flex flex-col space-y-4">
+          {
+        chatLog.map((message, index) => (
+          <div key={index} className={`flex ${
+            message.type === 'user' ? 'justify-end' : 'justify-start'
+            }`}>
+            <div className={`${
+              message.type === 'user' ? 'bg-purple-500' : 'bg-gray-800'
+            } rounded-lg p-4 text-white max-w-sm`}>
+            {message.message}
+            </div>
+            </div>
+        ))
+            }
+            {
+              isLoading &&
+              <div key={chatLog.length} className="flex justify-start">
+                  <div className="bg-gray-800 rounded-lg p-4 text-white max-w-sm">
+                    <TypingAnimation />
+                  </div>
+              </div>
+            }
+      </div>
+        </div>
+        <form onSubmit={handleSubmit} className="flex-none p-6">
+          <div className="flex rounded-lg border border-gray-700 bg-gray-800">  
+        <input type="text" className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none" placeholder="Type your message..." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+            <button type="submit" className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300">Send</button>
+            </div>
         </form>
-        </>
+        </div>
+    </div>
   )
 }
